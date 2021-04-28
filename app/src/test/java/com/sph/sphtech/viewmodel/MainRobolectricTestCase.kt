355551f7db6002d88fff369de21b51f113335d2a
Lixiaoxiang <>
2021-04-28 09:56:26 +0800

    change model test

--- app/src/test/java/com/sph/sphtech/viewmodel/MainRobolectricTestCase.kt
+++ app/src/test/java/com/sph/sphtech/viewmodel/MainRobolectricTestCase.kt
@@ -1,12 +1,14 @@
 package com.sph.sphtech.viewmodel
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.google.gson.stream.JsonReader
+import com.sp.sphtech.app.SPApplication
+import com.sp.sphtech.data.net.response.AllRecordResponse
 import com.sp.sphtech.vm.MainViewModel
 import com.sph.sphtech.BaseRobolectricTest
-import kotlinx.coroutines.*
 import org.junit.Assert
 import org.junit.Test
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
 
 class MainRobolectricTestCase : BaseRobolectricTest() {
 
@@ -16,18 +18,26 @@ class MainRobolectricTestCase : BaseRobolectricTest() {
     @Test
     @Throws(Exception::class)
     fun testMainViewModelDataLoad() {
-        GlobalScope.launch((Dispatchers.Main)) {
-            val model = MainViewModel()
-            model.getAllData()
-            val latch = CountDownLatch(1)
-            model.mListData.observeForever {
-                latch.countDown()
+        val model = MainViewModel()
+        model.mListData.observeForever {
+            model.mListData.value?.forEach {
+                println(it.quarter)
             }
-            withContext(Dispatchers.Unconfined) {
-                latch.await(10, TimeUnit.SECONDS)
-            }
-            Assert.assertNotNull(model.mListData.value)
         }
+        SPApplication.instance().assets.open("test.json").use { inputStream ->
+            JsonReader(inputStream.reader()).use { jsonReader ->
+                val records = object : TypeToken<AllRecordResponse>() {}.type
+                val plantList: AllRecordResponse = Gson().fromJson(jsonReader, records)
+                plantList.result?.records?.let {
+                    it.forEach { item ->
+                        item.year = item.quarter.split("-")[0].toInt()
+                    }
+                    model.mListData.value = it
+                }
+            }
+        }
+
+        Assert.assertNotNull(model.mListData.value)
     }
 
 
